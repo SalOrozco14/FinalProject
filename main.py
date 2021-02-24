e1aeec73be3f7135b40785846f090357ce7122c6
sorozcor <sorozcoramirez@ivytech.edu>
2021-02-24 19:18:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+"""
+Sprite
+---------
+Enemy - sal = alien - sal =ufo - paige = boss?
+
+player - PAIGE
+
+rockship - Paige 
+
+weapon? lazer change colors - PAIGE
+
+item: life pick up (red moon rock), weapon (yellow/blue moon rock) - find temp
+
+environment- platform? = pixal floor- temp
+
+backgroung img - PAIGE
+
+sounds: lazer, background music, blasting sound, boss sound when hit, item pick up =  Sal
+
+
+intro page - Sal
+==========
+explain story - saving his friend
+instructions
+start button
+
+
+player UML - paige
+===============
+var 
+===
+-life
++weapon grade
+
+method
+=======
+setWeapon
+setHealth
+getWeapon
+getHealth
+Jump
+shoot
+walk
+sound
+
+
+
+Enemy UML - sal
+=============
+var
+===
++Health
+
+method
+========
++StillAlive
+walk
+jump
+sound
+
+
+
+Boss UML *Extend from Enemy - sal
+========
+var
+==
++Health -OVERRIDER
++weapon
+
+method
+=======
+StillAlive
+walk 
+jump 
+shoot
+sound
+
+
+Documentation
+================
+https://docs.google.com/document/d/1dKxxUuqAPKO6HxcVrZH8hl6K6uM-d8vUfVP5f6Ou9FU/edit?usp=sharing
+
+"""
+
